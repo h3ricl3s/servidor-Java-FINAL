06fe73106e8c17097a5ec578cdb7db97d734cb31
Hericles Lopes <hericlesmilton@gmail.com>
2026-08-31 10:16:49 -0100

    o.1

--- src/main/java/com/labanta/servidorlocal/security/JwtService.java
+++ src/main/java/com/labanta/servidorlocal/security/JwtService.java
@@ -1,8 +1,49 @@
+
+//package com.labanta.servidorlocal.security;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.security.Keys;
+//import org.springframework.stereotype.Service;
+//
+//import javax.crypto.SecretKey;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Date;
+//
+//@Service
+//public class JwtService {
+//
+//    private static final String SEGREDO_TEXTO = "asdtgrnjytktuyjrynerte4n6547895698rbdxbt";
+//    private final SecretKey chaveSecreta = Keys.hmacShaKeyFor(
+//            "{JWT_SECRET}".getBytes(StandardCharsets.UTF_8
+//            ));
+//
+//    public String gerarToken(String username) {
+//        return Jwts.builder()
+//                .subject(username)
+//                .issuedAt(new Date())
+//                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+//                .signWith(chaveSecreta)
+//                .compact();
+//    }
+//
+//    public String extrairUsername(String token) {
+//        Claims claims = Jwts.parser()
+//                .verifyWith(chaveSecreta)
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload();
+//
+//        return claims.getSubject();
+//    }
+//}
+
 package com.labanta.servidorlocal.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -12,10 +53,13 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private static final String SEGREDO_TEXTO = "asdtgrnjytktuyjrynerte4n6547895698rbdxbt";
-    private final SecretKey chaveSecreta = Keys.hmacShaKeyFor(
-            "{JWT_SECRET}".getBytes(StandardCharsets.UTF_8
-            ));
+    private final SecretKey chaveSecreta;
+
+
+
+    public JwtService (@Value("${jwt.secret}")String segredo) {
+        this.chaveSecreta =Keys.hmacShaKeyFor(segredo.getBytes());
+    }
 
     public String gerarToken(String username) {
         return Jwts.builder()
@@ -36,3 +80,4 @@ public class JwtService {
         return claims.getSubject();
     }
 }
+
